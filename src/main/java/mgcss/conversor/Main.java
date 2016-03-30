f37e79e3da4a1b5adcd8507bbcf0fa0abbef738b
astrakel <kitiplas2002@hotmail.com>
2016-03-30 12:49:20 +0200

    Añado asteriscos

--- src/main/java/mgcss/conversor/Main.java
+++ src/main/java/mgcss/conversor/Main.java
@@ -10,6 +10,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println ("Bienvenido al convertidor de unidades de medida");
+        System.out.println ("***********************************************");
         // TODO aquí pondremos las llamadas a los métodos para convertir unidades
         /* ejemplo de llamada 
         double millas=0;
